8c6eea87ae0040ac8af90b91693debb9fda0ea91
will <williamgeorge.barker@gmail.com>
2022-07-09 01:17:17 -0400

    try non-flask code for invoke.py

--- invoke.py
+++ invoke.py
@@ -1,17 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 import os
-from flask import Flask
 
-app = Flask(__name__)
-script = 'script.sh'
-command = f'sh {script}'
+hostName = "0.0.0.0"
+serverPort = 8080
 
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.script = 'script.sh'
+        self.command = f'sh {self.script}'
+        os.system(self.command)
 
-@app.route("/")
-def invoke():
-    print('Hello!')
-    os.system(command)
-    return command
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    webServer.server_close()
+    print("Server stopped.")
+    
