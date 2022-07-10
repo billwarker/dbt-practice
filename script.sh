951480497a80edfba411342ec9ddecd1ceb672ff
will <williamgeorge.barker@gmail.com>
2022-07-10 07:06:21 -0400

    gsutil cp in script

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+dbt docs generate
+gsutil cp -r . gs://dbt-practice-upload
+
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
 dbt debug --target prod --profiles-dir .
