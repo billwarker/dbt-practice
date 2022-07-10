f29de5e1ce97f4b040c4c0bad4f95ec9496c12f8
will <williamgeorge.barker@gmail.com>
2022-07-10 07:16:55 -0400

    fix docs generate and gsutil cp

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-dbt docs generate
-gsutil cp -r . gs://dbt-practice-upload
 
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
 dbt debug --target prod --profiles-dir .
 dbt run --target prod --profiles-dir .
+dbt docs generate --profiles-dir .
+
+gsutil -m cp -r . gs://dbt-practice-upload
