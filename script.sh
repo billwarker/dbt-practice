23bcb077d09ec2dd827e4b9b54d138dafa9d667a
will <williamgeorge.barker@gmail.com>
2022-10-21 10:11:41 -0400

    update script for prod only

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-dbt debug --target dev --profiles-dir .
 dbt debug --target prod --profiles-dir .
 dbt run --target prod --profiles-dir .
 dbt docs generate --profiles-dir .
-
-gsutil -m cp -r . gs://dbt-practice-upload
